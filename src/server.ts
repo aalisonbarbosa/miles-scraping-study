2129fa26fdb94a206c40b9d690f8be2d53a875ac
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2026-09-13 03:08:56 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { connectDatabase } from "./config/db.js";
 import promotionsRouter from "./modules/promotions/promotion.routes.js";
 import { startScrapingJob } from "./jobs/scraping.job.js";
+import cors from "cors";
 import "dotenv/config.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/promotions", promotionsRouter);
 
